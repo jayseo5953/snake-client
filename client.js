b96dec7940617147c715c00034730ce11e6cc7fa
Jay Seo <sjs5953@hotmail.com>
2019-12-04 23:11:37 +0000

    client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+const connect = () => {
+  // grab the connection with the server
+  let conn = net.createConnection({
+    host:"192.168.88.149",
+    port: 50541
+  });
+
+  // interpret incoming data as texts
+  conn.setEncoding('utf8');
+
+  conn.on('data',(data) => {
+    console.log("The game says: ",data)
+  })
+
+  return conn;
+}
+module.exports.connect = connect
+console.log(module.exports)
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require('net');
-const connect = () => {
-  // grab the connection with the server
-  let conn = net.createConnection({
-    host:"192.168.88.149",
-    port: 50541
-  });
+const { connect } = require('./client');
 
-  // interpret incoming data as texts
-  conn.setEncoding('utf8');
+console.log('Connecting ...');
 
-  conn.on('data',(data) => {
-    console.log("The game says: ",data)
-  })
-
-  return conn;
-}
-
-console.log("Connecting....");
 connect();
