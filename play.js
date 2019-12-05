61370799a69a12e73ad16f35bcbb10d03b17e99c
Jay Seo <sjs5953@hotmail.com>
2019-12-05 00:22:52 +0000

    allowed taking in input

--- play.js
+++ play.js
@@ -2,4 +2,29 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 
+
+const HandleUserInput = (std) => {
+  std.on('data', data => {
+    if (data === '\u0003') {
+      console.log("bye bye")
+      process.exit();
+    }
+  })
+}
+
+
+const setUpInput = (callback) => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  callback(stdin);
+  return stdin
+};
 connect();
+
+setUpInput(HandleUserInput)
+
+
+
+
